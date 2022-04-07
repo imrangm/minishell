1f4ae7eb5e015c81e78add5cb789e2c3166c2d61
Nart <nmadi@student.42abudhabi.ae>
2022-04-07 13:24:32 +0400

    fix: Fixed segfault when ctrl+d in prompt with no text.

--- Nart/minishell.c
+++ Nart/minishell.c
@@ -6,7 +6,7 @@
 /*   By: nmadi <nmadi@student.42abudhabi.ae>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/04/07 13:05:40 by nmadi             #+#    #+#             */
-/*   Updated: 2022/04/07 13:18:49 by nmadi            ###   ########.fr       */
+/*   Updated: 2022/04/07 13:24:07 by nmadi            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,6 +19,8 @@ void	minishell(void)
 	while (1)
 	{
 		line = readline("$ ");
+		if (!line)
+			break ;
 		if (ft_strlen(line))
 			add_history(line);
 		free(line);
