a5cc4ccc95d72daa9f56215b9dc148d0d792eee5
Nart <nmadi@student.42abudhabi.ae>
2022-05-07 18:02:31 +0400

    feat: echo now strips off single quotes from the start and end.

--- srcs/b_echo.c
+++ srcs/b_echo.c
@@ -6,11 +6,41 @@
 /*   By: nmadi <nmadi@student.42abudhabi.ae>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/06 18:43:35 by nmadi             #+#    #+#             */
-/*   Updated: 2022/05/07 17:15:46 by nmadi            ###   ########.fr       */
+/*   Updated: 2022/05/07 18:01:34 by nmadi            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/minishell.h"
+
+void	ft_putchar(int c)
+{
+	write (1, &c, 1);
+}
+
+void	print_with_stripped_quotes(char *str, int space, int nl)
+{
+	int	i;
+	int	q;
+
+	i = 0;
+	q = 0;
+	if (str[0] == '\"' || str[0] == '\'')
+	{
+		i++;
+		q = 1;
+	}
+	while (str[i])
+	{
+		if (i == (int) ft_strlen(str) - 1 && q)
+			break ;
+		ft_putchar(str[i]);
+		i++;
+	}
+	if (nl)
+		ft_putchar('\n');
+	if (space)
+		ft_putchar(32);
+}
 
 int	b_echo(char **args)
 {
@@ -24,11 +54,11 @@ int	b_echo(char **args)
 	while (args[i])
 	{
 		if (!args[i + 1] && !normal_mode)
-			printf("%s", args[i]);
+			print_with_stripped_quotes(args[i], 0, 0);
 		else if (!args[i + 1] && normal_mode)
-			printf("%s\n", args[i]);
+			print_with_stripped_quotes(args[i], 0, 1);
 		else
-			printf("%s ", args[i]);
+			print_with_stripped_quotes(args[i], 1, 0);
 		i++;
 	}
 	return (0);
