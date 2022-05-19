490bafbe774a33b30d57bce82b583181931dcf6a
Nart <nmadi@student.42abudhabi.ae>
2022-05-19 16:08:46 +0400

    removed 0

--- srcs/b_env.c
+++ srcs/b_env.c
@@ -6,7 +6,7 @@
 /*   By: nmadi <nmadi@student.42abudhabi.ae>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/04/30 19:19:34 by nmadi             #+#    #+#             */
-/*   Updated: 2022/05/19 15:59:35 by nmadi            ###   ########.fr       */
+/*   Updated: 2022/05/19 16:08:25 by nmadi            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,7 +25,7 @@ int	b_env(char **envp, int export_mode)
 		{
 			printf("declare -x %s=", get_export_value_side(envp[i], 1));
 			printf("\"%s\"\n", get_export_value_side(envp[i], 0));
-		}0
+		}
 		else if (ft_strchr(envp[i], '=') && !export_mode)
 			printf("%s\n", envp[i]);
 		i++;
