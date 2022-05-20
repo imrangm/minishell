bfe650bcfd40da8743680c378453b9afe2b0aff3
Nart <unconfigured@null.spigotmc.org>
2022-05-20 16:28:21 +0400

    fix: cmp_cmd()

--- srcs/cmp_cmd.c
+++ srcs/cmp_cmd.c
@@ -6,7 +6,7 @@
 /*   By: nmadi <nmadi@student.42abudhabi.ae>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/19 15:23:41 by nmadi             #+#    #+#             */
-/*   Updated: 2022/05/19 15:32:28 by nmadi            ###   ########.fr       */
+/*   Updated: 2022/05/20 16:27:25 by nmadi            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,7 +23,7 @@ int	cmp_cmd(char *cmd, char *builtin)
 			return (1);
 		i++;
 	}
-	if (!cmd[i] && builtin[i])
+	if (cmd[i] || builtin[i])
 		return (1);
 	return (0);
 }
