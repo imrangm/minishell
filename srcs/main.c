0fb83fb99c2087103f0fd665789c44d6d5b36132
Nart <42nmadi@gmail.com>
2022-05-27 14:14:49 +0400

    refactor: main.c

--- srcs/main.c
+++ srcs/main.c
@@ -6,20 +6,26 @@
 /*   By: nmadi <nmadi@student.42abudhabi.ae>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/04/08 16:25:02 by imustafa          #+#    #+#             */
-/*   Updated: 2022/05/26 18:49:15 by nmadi            ###   ########.fr       */
+/*   Updated: 2022/05/27 14:13:31 by nmadi            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/minishell.h"
 
-int	main(int argc, char **argv, char **envp)
+void	execute_line(char *line, t_data *data)
+{
+	if (ft_strchr(line, '|'))
+		split_pipe(line, data);
+	else if (ft_strchr(line, '<') || ft_strchr(line, '>'))
+		split_rd(line, data);
+	else
+		master_execute(line, data);
+}
+
+void	minishell(t_data *data)
 {
 	char	*line;
-	t_data	data;
 
-	(void) argc;
-	(void) argv;
-	data.envp = init_envp(envp);
 	in_minishell_var(1);
 	while (isatty(STDIN_FILENO))
 	{
@@ -28,25 +34,28 @@ int	main(int argc, char **argv, char **envp)
 		line = readline("$> ");
 		if (!line)
 		{
-			ft_free_arg(data.envp);
+			ft_free_arg(data->envp);
 			break ;
 		}
 		if (line[0])
 		{
 			add_history(line);
-			if (preliminary_check(line, &data))
-			{
-				if (ft_strchr(line, '|'))
-					split_pipe(line, &data);
-				else if (ft_strchr(line, '<') || ft_strchr(line, '>'))
-					split_rd(line, &data);
-				else
-					master_execute(line, &data);
-			}
+			if (preliminary_check(line, data))
+				execute_line(line, data);
 		}
 		else
-			data.last_exit_status = 0;
+			data->last_exit_status = 0;
 		in_minishell_var(1);
 		ft_free(line);
 	}
+}
+
+int	main(int argc, char **argv, char **envp)
+{
+	t_data	data;
+
+	(void) argc;
+	(void) argv;
+	data.envp = init_envp(envp);
+	minishell(&data);
 }
