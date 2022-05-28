c5fb26773873ba2e79f6c11c3447ced71fef37e6
Nart <42nmadi@gmail.com>
2022-05-28 12:40:29 +0400

    feat: minishell initializes the right set of env vars if it is run in env -i mode.

--- srcs/init_envp.c
+++ srcs/init_envp.c
@@ -6,7 +6,7 @@
 /*   By: nmadi <nmadi@student.42abudhabi.ae>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/23 15:19:28 by nmadi             #+#    #+#             */
-/*   Updated: 2022/05/27 14:35:21 by nmadi            ###   ########.fr       */
+/*   Updated: 2022/05/28 12:39:04 by nmadi            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,7 +20,15 @@ char	**init_envp(char **envp)
 	cwd = NULL;
 	cloned_envp = NULL;
 	cloned_envp = clone_env(envp, 0);
-	cloned_envp = add_env(ft_strdup("PWD"),
+	if (!cloned_envp[0])
+	{
+		cloned_envp = add_env("_", "/usr/bin/env", cloned_envp);
+		cloned_envp = add_env("SHLVL", "1", cloned_envp);
+	}
+	else
+		cloned_envp = add_env("_",
+			ft_strjoin(getcwd(cwd, sizeof(cwd)), "minishell"), cloned_envp);
+	cloned_envp = add_env("PWD",
 			getcwd(cwd, sizeof(cwd)), cloned_envp);
 	return (cloned_envp);
 }
