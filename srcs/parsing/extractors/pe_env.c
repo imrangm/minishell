3b21451941cf135b817b76d8b267672569c37c96
Nart <42nmadi@gmail.com>
2022-08-19 11:26:14 +0000

    init_envp() is functional now without leaks.

--- srcs/parsing/extractors/pe_env.c
+++ srcs/parsing/extractors/pe_env.c
@@ -6,58 +6,55 @@
 /*   By: nmadi <nmadi@student.42abudhabi.ae>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/23 15:19:28 by nmadi             #+#    #+#             */
-/*   Updated: 2022/08/16 18:54:34 by nmadi            ###   ########.fr       */
+/*   Updated: 2022/08/19 11:25:22 by nmadi            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../../includes/minishell.h"
 
-// static void	set_shlvl(t_data *data)
-// {
-// 	int		shlvl;
-// 	char	*shlvl_rhs;
-// 	char	*shlvl_itoa;
+static void	set_shlvl(t_data *data)
+{
+	int		shlvl;
+	char	*shlvl_rhs;
+	char	*shlvl_itoa;
 
-// 	shlvl = 0;
-// 	shlvl_rhs = NULL;
-// 	shlvl_itoa = NULL;
-// 	if (env_exists("SHLVL", data))
-// 	{
-// 		shlvl_rhs = get_env_value("SHLVL", data);
-// 		if (!ft_aredigits(shlvl_rhs))
-// 			modify_env(ft_strdup("SHLVL"), "1", data);
-// 		else
-// 		{
-// 			shlvl = ft_atoi(shlvl_rhs) + 1;
-// 			shlvl_itoa = ft_itoa(shlvl);
-// 			modify_env(ft_strdup("SHLVL"), shlvl_itoa, data);
-// 		}
-// 	}
-// 	else
-// 		modify_env(ft_strdup("SHLVL"), "1", data);
-// 	ft_free(shlvl_rhs);
-// }
+	shlvl = 0;
+	shlvl_rhs = NULL;
+	shlvl_itoa = NULL;
+	if (env_exists("SHLVL", data))
+	{
+		shlvl_rhs = get_env_value("SHLVL", data);
+		if (!ft_aredigits(shlvl_rhs))
+			modify_env(ft_strdup("SHLVL"), "1", data);
+		else
+		{
+			shlvl = ft_atoi(shlvl_rhs) + 1;
+			shlvl_itoa = ft_itoa(shlvl);
+			modify_env(ft_strdup("SHLVL"), shlvl_itoa, data);
+		}
+	}
+	else
+		modify_env(ft_strdup("SHLVL"), "1", data);
+	ft_free(shlvl_rhs);
+}
 
 void	init_envp(char **envp, t_data *data)
 {
-	// char	*cwd;
-
-	// cwd = NULL;
 	data->pwd = NULL;
 	data->old_pwd = NULL;
 	data->envp = clone_env(envp, 0);
-	// delete_env("OLDPWD", data);
-	// if (!data->envp[0])
-	// {
-	// 	modify_env(ft_strdup("OLDPWD"), NULL, data);
-	// 	modify_env(ft_strdup("SHLVL"), "1", data);
-	// 	if (!env_exists("_", data))
-	// 		modify_env("_", "/usr/bin/env", data);
-	// }
-	// else
-	// {
-	// 	append_env(ft_strdup("OLDPWD"), NULL, data);
-	// 	set_shlvl(data);
-	// }
-	// modify_env(ft_strdup("PWD"), getcwd(cwd, sizeof(cwd)), data);
+	delete_env("OLDPWD", data);
+	if (!data->envp[0])
+	{
+		modify_env(ft_strdup("OLDPWD"), NULL, data);
+		modify_env(ft_strdup("SHLVL"), "1", data);
+		if (!env_exists("_", data))
+			modify_env("_", "/usr/bin/env", data);
+	}
+	else
+	{
+		append_env(ft_strdup("OLDPWD"), NULL, data);
+		set_shlvl(data);
+	}
+	modify_env(ft_strdup("PWD"), getcwd(NULL, 0), data);
 }
