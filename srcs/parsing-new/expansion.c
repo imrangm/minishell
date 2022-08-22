8c810dcdcb51b1f229c4df68cc7d4130a52f4f9c
Imran <imzs@hotmail.com>
2022-08-22 11:01:03 +0400

    supports multiple expansions

--- srcs/parsing-new/expansion.c
+++ srcs/parsing-new/expansion.c
@@ -6,7 +6,7 @@
 /*   By: imustafa <imustafa@student.42abudhabi.ae>  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/08/20 15:51:17 by imustafa          #+#    #+#             */
-/*   Updated: 2022/08/21 17:10:20 by imustafa         ###   ########.fr       */
+/*   Updated: 2022/08/22 10:43:52 by imustafa         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -46,9 +46,14 @@
 // 	loc->right_node = end;
 // 	return (pair_node(loc, param, "EXPANSION"));
 // }
+// char	*expand(char *args, int start, int end, int len)
+// {
+	
+// }
 
 void	expander(t_node	*cmd, t_data *data)
 {
+	t_node		*current;
 	char		*param;
 	char		*final;
 	int			start;
@@ -57,38 +62,51 @@ void	expander(t_node	*cmd, t_data *data)
 	size_t		i;
 	size_t		j;
 
-	if (ft_strncmp(cmd->right_node->right_node->value, "?", 1) == 0)
-		param = ft_itoa(data->last_exit_status);
-	else
-		param = get_env_value(cmd->right_node->right_node->value, data);
-	start = cmd->right_node->left_node->left_node->val;
-	end = cmd->right_node->left_node->right_node->val;
-	len = ft_strlen(param) + ft_strlen(cmd->left_node->value) - (end - start);
-	printf("values start:%d, end:%d, len:%d\n", start, end, len);
-	printf("param: %s\n", param);
-	final = malloc(sizeof(char) * len);
-	i = 0;
-	while (i < (size_t) start)
+	current = cmd->right_node;
+	param = NULL;
+	while (current && current->type == 1)
 	{
-		final[i] = cmd->left_node->value[i];
-		i++;
+		//expand next parameter
+		start = current->left_node->left_node->val;
+		end = current->left_node->right_node->val;
+		if (param)
+		{
+			start += (ft_strlen(param) - ft_strlen(current->value) - 1);
+			end += (ft_strlen(param) - ft_strlen(current->value) - 1);
+		}
+		if (ft_strncmp(current->right_node->value, "?", 1) == 0)
+			param = ft_itoa(data->last_exit_status);
+		else
+			param = get_env_value(current->right_node->value, data);
+		len = ft_strlen(param) + ft_strlen(cmd->left_node->value) - (end - start);
+		printf("values start:%d, end:%d, len:%d\n", start, end, len);
+		printf("param: %s\n", param);
+		final = malloc(sizeof(char) * len);
+		i = 0;
+		while (i < (size_t) start)
+		{
+			final[i] = cmd->left_node->value[i];
+			i++;
+		}
+		j = 0;
+		while (j < ft_strlen(param))
+		{
+			final[i] = param[j];
+			i++;
+			j++;
+		}
+		j = end + 1;
+		while (j < ft_strlen(cmd->left_node->value))
+		{
+			final[i] = cmd->left_node->value[j];
+			i++;
+			j++;
+		}
+		final[i] = '\0';
+		printf("final: %s\n", final);
+		// ft_free(cmd->left_node->value);
+		cmd->left_node->value = ft_strdup(final);
+		cmd->left_node->id = ft_strdup("FINAL");
+		current = current->right_node;
 	}
-	j = 0;
-	while (j < ft_strlen(param))
-	{
-		final[i] = param[j];
-		i++;
-		j++;
-	}
-	j = end + 1;
-	while (j < ft_strlen(cmd->left_node->value))
-	{
-		final[i] = cmd->left_node->value[j];
-		i++;
-		j++;
-	}
-	final[i] = '\0';
-	printf("final: %s\n", final);
-	cmd->left_node->value = final;
-	cmd->left_node->id = ft_strdup("FINAL");
 }
